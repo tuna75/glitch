b9e69e77b10354dc6010ae907e2f441b779a6138
tuna75 <47067541+tuna75@users.noreply.github.com>
2019-02-13 11:23:40 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x google-chrome-stable_current_amd64.deb /tmp/dir
 rm google-chrome-stable_current_amd64.deb
 pnpm i
-npm start && rm -- $0
+rm -- $0
