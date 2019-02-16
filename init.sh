91104da6d8152d073c637d4d784d6135a19694b4
tuna75 <47067541+tuna75@users.noreply.github.com>
2019-02-16 12:30:12 -0600

    Delete init.sh

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -x google-chrome-stable_current_amd64.deb /tmp/dir
-rm google-chrome-stable_current_amd64.deb
